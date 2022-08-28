3c3441fee09aa4b5579a268c6ca05730e4dfb57c
Dukler <iarwain@hotmail.com.ar>
2022-08-28 02:32:08 -0300

    asd

--- src/bot/index.js
+++ src/bot/index.js
@@ -65,7 +65,7 @@ const executeUtilsCommand = (command, commandOption, interaction) => {
 
             return true;
         case 'ping':
-            const path = __dirname + '../utils/ping.sh'
+            const path = __dirname + '/../utils/ping.sh'
             // runRemote({run:'/home/comanchero-s0/Documents/ping.sh', commandOption, username, host})
             // const asd = require('../utils/ping.sh')
             runLocal({run:[path,String(host)]})
